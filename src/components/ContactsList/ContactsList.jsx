b78bfedb4f72ce94500d9139ea2051f9fbb2c7b4
Rafał Gałecki <darthunter@gmail.com>
2023-03-28 20:52:19 +0200

    class to function

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,21 +1,16 @@
-import { Component } from 'react';
 import css from './ContactsList.module.css';
 import PropTypes from 'prop-types';
 
-class ContactsList extends Component {
-  render() {
-    const { contactsLength, children } = this.props;
-
-    return (
-      <div className={css.contactsListBox}>
-        <h4>
-          You have {contactsLength} contact{contactsLength === 1 ? null : 's'}
-        </h4>
-        <ul className={css.contactsList}>{children}</ul>
-      </div>
-    );
-  }
-}
+const ContactsList = ({ contactsLength, children }) => {
+  return (
+    <div className={css.contactsListBox}>
+      <h4>
+        You have {contactsLength} contact{contactsLength === 1 ? null : 's'}
+      </h4>
+      <ul className={css.contactsList}>{children}</ul>
+    </div>
+  );
+};
 
 ContactsList.propTypes = {
   children: PropTypes.node,
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,26 +1,23 @@
-import { Component } from 'react';
+//import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-class Filter extends Component {
-  render() {
-    const { value, onChange } = this.props;
-    const filterId = nanoid();
-    return (
-      <div className={css.filter}>
-        <label htmlFor={filterId}>Find contacts by name</label>
-        <input
-          className={css.inputFilter}
-          id={filterId}
-          type="search"
-          value={value}
-          onChange={onChange}
-        ></input>
-      </div>
-    );
-  }
-}
+const Filter = ({ value, onChange }) => {
+  const filterId = nanoid();
+  return (
+    <div className={css.filter}>
+      <label htmlFor={filterId}>Find contacts by name</label>
+      <input
+        className={css.inputFilter}
+        id={filterId}
+        type="search"
+        value={value}
+        onChange={onChange}
+      ></input>
+    </div>
+  );
+};
 
 Filter.propTypes = {
   value: PropTypes.string,
